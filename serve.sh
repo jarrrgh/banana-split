7eefb64f8697f974dc62551a7170eccd0e572d1e
Jari Suksi <jari.suksi@revonte.com>
2023-05-01 17:42:41 +0300

    Update deploy script paths.

--- serve.sh
+++ serve.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+SCRIPT_DIR="$(dirname $0)"
+
 #PLUGINS_PATH="$HOME/Library/Application Support/cura/4.12/plugins/"
 PLUGINS_PATH="$HOME/Library/Application Support/cura/5.3/plugins/"
 DEPLOY_PATH="$PLUGINS_PATH/BananaSplit"
@@ -9,11 +11,11 @@ mkdir -p "$DEPLOY_PATH"
 
 echo
 echo "Prepare BananaSplit"
-zip -r BananaSplit.plugin BananaSplit/ -x "*.DS_Store"
+zip -r "${SCRIPT_DIR}/BananaSplit.plugin" "${SCRIPT_DIR}/BananaSplit/" -x "*.DS_Store"
 
 # echo
 echo "Serve BananaSplit"
-unzip -o BananaSplit.plugin -d "$DEPLOY_PATH"
+unzip -o "${SCRIPT_DIR}/BananaSplit.plugin" -d "$DEPLOY_PATH"
 
 echo
 echo "Restart Cura"
@@ -23,7 +25,7 @@ killall UltiMaker-Cura
 sleep 1
 
 # Open the test file after 10 seconds
-#sleep 10 && open -a Ultimaker\ Cura.app banana-split.3mf &
+#sleep 10 && open -a Ultimaker\ Cura.app ${SCRIPT_DIR}/banana-split.3mf &
 
 # Open Cura for debug 4.xx
 #/Applications/Ultimaker\ Cura.app/Contents/MacOS/cura --debug
