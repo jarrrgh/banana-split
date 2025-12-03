71230137271bf889031f1f345c35c8c9c8b23cc1
Jari Suksi <jarrrgh@gmail.com>
2025-12-03 23:54:53 +0200

    Add note to self

--- package.sh
+++ package.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # Apparently relative paths mess up zip files, so run this in the project root.
+# Note to self: for some reason this did not work and I had to unpack and repack the zip.
 
 echo
 echo "Compressing..."
